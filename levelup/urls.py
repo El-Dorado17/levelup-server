cd91ce77f7f94a5350325accdc18a434595098b3
Ely Dorado <2018dorado@gmail.com>
2023-05-10 11:56:58 -0500

    mid-day push

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,14 +2,23 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
+from django.conf.urls import include
+from rest_framework import routers
+from levelupapi.views import GameTypeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
 
 #register
 # Requests to http://localhost:8000/register will be routed to the register_user function
 #login
 # Requests to http://localhost:8000/login will be routed to the login_user function
+
---
+++ notes.py
@@ -0,0 +1,6 @@
+
+
+#Make sure on every startup, pipenv shell is run on the terminal to setup the virtual environment!
+
+#? Starts Server!
+# python3 manage.py runserver
